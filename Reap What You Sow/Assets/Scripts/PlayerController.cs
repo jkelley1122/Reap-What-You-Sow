1a120083b8e6a476dba103ca41459e0ffd8e19bb
John <68163612+Johnathy@users.noreply.github.com>
2023-09-11 01:13:25 -0400

    Update PlayerController.cs
    added some comments and extra spaces to make script more readable

--- Reap What You Sow/Assets/Scripts/PlayerController.cs
+++ Reap What You Sow/Assets/Scripts/PlayerController.cs
@@ -23,23 +23,33 @@ public class PlayerController : MonoBehaviour
     }
     void Start()
     {
+        //any movement input will allow the character to be followed by the camera
         input = Camera.main.transform.forward;
-        input.y = 0;
+
+        input.y = 0; //locks the y-axis so there's no floating or other funny business with gravity.  Keeps player grounded. 
+
         input = Vector3.Normalize(input);
+
         rotate = Quaternion.Euler(new Vector3(0, 90, 0)) * input;
 
 
     }
     void GetMovement()
     {
+        //direction is what acquires the input from the keyboard.  Unity defaults to WASD config for movement.  Add rebindable keys later.
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        
         Vector3 righttMovement = rotate * speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        
         Vector3 upwardMovement = input * speed * Time.deltaTime * Input.GetAxis("Vertical");
 
         //tracks the direction the player is currently going toward
         Vector3 currentDirection = Vector3.Normalize(righttMovement + upwardMovement);
+
         transform.forward = currentDirection;
+
         transform.position += righttMovement;
+
         transform.position += upwardMovement;
 
     }
