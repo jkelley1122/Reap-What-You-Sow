892972e36ca308e45deecc1af7412bf7c47e4d0e
Sean Blankenship <seanblankenship2002@gmail.com>
2023-10-04 16:45:37 -0400

    Fixed my import of Brendan's Farming
    No clue what happened but somehow I reverted everything back to the old farming system. This has been fixed, I also revised how the sprites were imported as it's an admittedly confusing process but just for reference here's what I did.
    
    1.) In the Sprite3D of the plant node, under flags, I Enabled Billboard.
    2.) In that same area, I set the Texture Filter to "Nearest"
    3.) I made sure that with all of the sprites that have been imported, that they were imported as Texture2D assets with their compression mode set to Lossless. This is done in a different tab next to the Scene menu around the top left.

--- Reap What You Sow/Assets/Scripts/Interactables/NPCController.gd
+++ Reap What You Sow/Assets/Scripts/Interactables/NPCController.gd
@@ -1,12 +1,14 @@
 extends CharacterBody3D
 
 @export var npc_name = "Rana"
-@export var npc_reputation = 100
 
 @onready var dialogue_menu = $DialogueMenu
 @onready var animator: AnimationPlayer = $DialogueMenu/AnimationPlayer
-	
+
+func _ready():
+	match npc_name:
+		"Rana":
 func talk():
 	if (dialogue_menu.has_method("start_dialogue")):
-		dialogue_menu.start_dialogue("Rana", 95)
+		dialogue_menu.start_dialogue(npc_name)
 
