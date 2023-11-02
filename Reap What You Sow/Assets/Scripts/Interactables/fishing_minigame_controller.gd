d10a8535faa9ba795e369386335abae7d3236a6c
John <john_kelley66@mymail.eku.edu>
2023-11-02 05:31:15 -0400

    Beta Fishing
    -Added fishing functionality
    -currently in "beta" stage where the fish is simply given by a random number gen

--- Reap What You Sow/Assets/Scripts/Interactables/fishing_minigame_controller.gd
+++ Reap What You Sow/Assets/Scripts/Interactables/fishing_minigame_controller.gd
@@ -1,25 +1,20 @@
 extends Area3D
 
 @onready var player = get_tree().current_scene.get_node("Player")
-
+var random = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-
-
-
-
-
 
 func setup_fishing(area):
-	if player.is_in_group("player"):         #trigger only when the player enters the trigger zone
-		player.inventory.add_item("Fish", 1) #adds fish to inventory
-		player.position.x = -3.289           #centers the player on the x and z axis.  temporary?
-		player.position.z = -0.991
+	if player.is_in_group("player"):              #trigger only when the player enters the trigger zone
+		var rng = random.randi_range(0, 3)        #you have a 1 in 3 chance of catching a fish currently. Temp?
+		if rng == 1:
+			player.inventory.add_item("Fish", 1)  #adds fish to inventory
+			print("Fish Get!")                    #debug line
+#randi_range is indexed, in this case 0 - 4
